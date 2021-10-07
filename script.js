f51cb76ff189f03f08bae7a4b0f40b1efee29a8c
SrinivasaRaviAhlad <raviaahlad@gmail.com>
2021-10-07 20:11:59 +0530

    application completed

--- script.js
+++ script.js
@@ -18,20 +18,87 @@ function getAllDateFormats(date) {
   const ddmmyyyy = date.day + date.month + date.year;
   const mmddyyyy = date.month + date.day + date.year;
   const yyyymmdd = date.year + date.month + date.day;
-  const ddmmyy = date.day + date.month + date.year.slice(-2);
-  const mmddyy = date.month + date.day + date.year.slice(-2);
-  const yymmdd = date.year.slice(-2) + date.month + date.day;
-  return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+  // const ddmmyy = date.day + date.month + date.year.slice(-2);
+  // const mmddyy = date.month + date.day + date.year.slice(-2);
+  // const yymmdd = date.year.slice(-2) + date.month + date.day;
+  return [ddmmyyyy, mmddyyyy, yyyymmdd];
+}
+
+function isLeapYear(year) {
+  if (year % 400 === 0) {
+    return true;
+  }
+  if (year % 100 === 0) {
+    return false;
+  }
+  if (year % 4 === 0) {
+    return true;
+  }
+  return false;
+}
+
+function getNextDate(date) {
+  let day = Number(date.day);
+  let month = Number(date.month);
+  let year = Number(date.year);
+
+  const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  if (month === 2) {
+    //if month is febrauary
+    if (isLeapYear(year)) {
+      // if year is leap year
+      if (day < 29) {
+        day++;
+      } else {
+        day = 1;
+        month++;
+      }
+    } else {
+      //not leap year
+      if (day < daysInMonth[month - 1]) {
+        day++;
+      } else {
+        day = 1;
+        month++;
+      }
+    }
+  } else {
+    if (day < daysInMonth[month - 1]) {
+      day++;
+    } else {
+      day = 1;
+      month++;
+    }
+
+    if (month > 12) {
+      day = 1;
+      month = 1;
+      year++;
+    }
+  }
+  day = day < 10 ? '0' + String(day) : String(day);
+  month = month < 10 ? '0' + String(month) : String(month);
+  return { day, month, year: String(year) };
 }
 
 function isDatePalindrom(date) {
   const dateFormats = getAllDateFormats(date);
   for (i of dateFormats) {
     if (isPalindrom(i)) {
-      return `Yah date is palindrom ✨🎉 ${i}`;
+      return true;
     }
   }
-  return 'Nah date is not a palindrom 😏';
+}
+
+function getNextPalindromDate(date) {
+  let cnt = 0;
+  while (!isDatePalindrom(date)) {
+    console.log(date);
+    cnt++;
+    date = getNextDate(date);
+  }
+  return [date, cnt];
 }
 
 const date = {
@@ -39,15 +106,22 @@ const date = {
   month: '',
   year: '',
 };
+/*element selector*/
 const bdayInput = document.querySelector('#bday-input');
 const showBtn = document.querySelector('#show-btn');
 const showOutput = document.querySelector('#result');
-function btnHandler(e) {
+
+function btnHandler() {
   const userInput = bdayInput.value.split('-');
   date.day = userInput[2];
   date.month = userInput[1];
   date.year = userInput[0];
-  showOutput.innerText = isDatePalindrom(date);
+  if (isDatePalindrom(date)) {
+    showOutput.innerText = `Yah date is palindrom 🤩 ${date.day}-${date.month}-${date.year}`;
+  } else {
+    [nextDate, count] = getNextPalindromDate(date);
+    showOutput.innerText = `Next Palindrome date is ${nextDate.day}-${nextDate.month}-${nextDate.year} ${count} days away`;
+  }
 }
 
 showBtn.addEventListener('click', btnHandler);
